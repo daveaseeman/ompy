bffd4ffc08fb5341b6472e0b58c841a1ad620844
Dave Seeman <daveseeman@icloud.com>
2017-05-03 00:22:29 -0500

    week4 weather app

--- week4/myapp/myapp.py
+++ week4/myapp/myapp.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from weather import get_forecast
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    address = request.values.get('address')
+    forecast = None
+    print(address)
+    if address:
+        forecast = get_forecast(address).decode()
+        print(forecast)
+    return render_template('index.html', forecast=forecast)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
